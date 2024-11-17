e8edbf2d4193bb79d2594cb1ca755db60aaac400
orash <orash.axmedov000@gmail.com>
2024-11-17 09:45:58 +0500

    Accordion fixes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,33 @@
-const menuOpen = document.querySelector('.menu-open');
-const menuClose = document.querySelector('.menu-close');
-const menu = document.querySelector('.menu');
+const menuOpen = document.querySelector('.menu-open')
+const menuClose = document.querySelector('.menu-close')
+const menu = document.querySelector('.menu')
 
-const accordionBtn = document.querySelectorAll('.accordion__btn');
-const accordionInfo = document.querySelectorAll('.accordion__info');
+const accordionBtn = document.querySelectorAll('.accordion__btn')
+const accordionInfo = document.querySelectorAll('.accordion__info')
 
-const footerYear = document.querySelector('.footer__year');
+const footerYear = document.querySelector('.footer__year')
 
 // Burger Menu
-menuOpen.addEventListener('click', () => menu.classList.toggle('hidden'));
-menuClose.addEventListener('click', () => menu.classList.toggle('hidden'));
+menuOpen.addEventListener('click', () => menu.classList.toggle('hidden'))
+menuClose.addEventListener('click', () => menu.classList.toggle('hidden'))
 
 // Accordion
 accordionBtn.forEach((item) => {
-  item.addEventListener('click', () => accordionInfo.forEach((item) => item.classList.toggle('hidden')));
-});
+  item.addEventListener('click', (event) => {
+    const currentAccordion = event.target.closest('.accordion')
+    const currentAccordionInfo = currentAccordion.querySelector('.accordion__info')
+    
+    accordionInfo.forEach((info) => {
+      if (info !== currentAccordionInfo) {
+        info.classList.add('hidden')
+      }
+    })
+
+    if (currentAccordionInfo.classList.contains('hidden')) {
+      currentAccordionInfo.classList.remove('hidden')
+    }
+  })
+})
 
 // Current Year
-footerYear.textContent = new Date().getFullYear();
+footerYear.textContent = new Date().getFullYear()
